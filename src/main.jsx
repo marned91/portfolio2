8481d8ebecb7f5317406f07a7e56b1979f6c9562
marned91 <152280051+marned91@users.noreply.github.com>
2025-10-11 16:18:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </HashRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
